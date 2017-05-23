cedc7b874ee31df1093b3051deab8698faa055fb
Yhorman Perez <jhppunk@gmail.com>
2017-05-22 22:32:45 -0500

    until a service was added on product list component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,16 @@
 
 import {Component} from '@angular/core';
 
+import { ProductService } from './products/product.service';
+
 @Component ({
     selector: 'pm-app',
     template: `
     <div><h1>{{pageTitle}}</h1>
         <pm-products></pm-products>
     </div>
-    `
+    `,
+    providers : [ProductService ]
 })
 
 export class AppComponent{
