4c32fe2c54f49a57eb79961bf04f9b12796efc64
Yhorman Perez <jhppunk@gmail.com>
2017-05-31 17:29:44 -0500

    until navigation and routing basics

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,21 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+
 import { ProductListComponent} from  './products/product-list.component';
 import { ProductFilterPipe} from './products/product-filter.pipe';
+import { ProductDetailComponent } from './products/product-detail.component';
+
+
 import { StarComponent } from './shared/star.component'
 
 @NgModule({
   imports: [ BrowserModule,
    FormsModule, 
-   HttpModule 
+   HttpModule,
+   RouterModule.forRoot([
+     { path:'products', component:ProductListComponent },
+     { path:'product/:id', component:ProductDetailComponent },
+     { path:'welcome', component: WelcomeComponent },
+     { path:'', redirectTo : 'welcome' , pathMatch:'full' },
+     { path:'**', redirectTo : 'welcome' , pathMatch:'full' }
+   ])
    ],
-  declarations: [ AppComponent,
-   ProductListComponent,
-   ProductFilterPipe,
-   StarComponent
+  declarations: [ 
+    AppComponent,
+    WelcomeComponent,
+    ProductListComponent,
+    ProductFilterPipe,
+    ProductDetailComponent,
+    StarComponent
     ],
   bootstrap: [ AppComponent ]
 })
